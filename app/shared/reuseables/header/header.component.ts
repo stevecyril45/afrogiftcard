14cea8bdd8e6830ae76863ea52617f5937325926
Jamike <jamikeobikwelu@gmail.com>
2025-07-08 22:27:35 +0100

    Made adjustments to the responsiveness

--- app/shared/reuseables/header/header.component.ts
+++ app/shared/reuseables/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -12,5 +14,19 @@ isMobileMenuOpen = false;
 toggleMobileMenu() {
   this.isMobileMenuOpen = !this.isMobileMenuOpen;
 }
+
+@ViewChild('sidebarElement') sidebar!: ElementRef;
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        // Remove sidebar classes on navigation
+        this.sidebar.nativeElement.classList.remove('active');
+        document.body.classList.remove('sidebar-open');
+      });
+  }
 
 }
